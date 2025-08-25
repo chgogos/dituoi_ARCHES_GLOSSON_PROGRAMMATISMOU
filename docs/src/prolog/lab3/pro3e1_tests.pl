5680727cd31c569d6abda446677dc08e7b2f0032
Christos Gogos <chgogos@gmail.com>
2025-08-25 15:01:27 +0300

    june2025

--- docs/src/prolog/lab3/pro3e1_tests.pl
+++ docs/src/prolog/lab3/pro3e1_tests.pl
@@ -5,7 +5,7 @@ test(male_john) :- male(john).
 
 test(female_mary) :- female(mary).
 
-test(john_is_parent_of_mary) :- parent(john, mary).
+test(john_is_parent_of_mary) :- parent(mary, john).
 
 
 test(direct_ancestor, [nondet]) :-  ancestor(john, mary).
